48a6bcc36442c0f1a6af307103bb8be1a145be29
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-02-27 02:08:48 +0900

    TrafficLightを初期化

---
+++ Assets/Scripts/InGame/Managers/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        //全TrafficLightsSystemが初期化済みか
+        private bool intersectionIsInitialized = false;
+
+        private void Update()
+        {
+            //TrafficLightsSystemを初期化
+            //Start()に置くとRoad接続に先回りしてしまうためここに配置
+            if (!intersectionIsInitialized)
+            {
+                InitilizeTrafficLightsSystems();
+            }
+        }
+
+        /// <summary>
+        /// 全道路が接続済みか確認して、接続済みなら全TrafficLightsSystemを初期化させる
+        /// </summary>
+        private void InitilizeTrafficLightsSystems()
+        {
+            //未接続の道路が存在していればキャンセル
+            Road[] allRoads = FindObjectsOfType<Road>();
+            foreach(Road road in allRoads)
+            {
+                if (!road.isInitialized)
+                {
+                    //>>未接続
+                    return;
+                }
+            }
+
+            //>>全道路が接続済み
+
+            //全TrafficLightsSystemを初期化させる
+            Intersection[] allIntersections = FindObjectsOfType<Intersection>();
+            foreach(Intersection intersection in allIntersections) 
+            {
+                intersection.InitializeTrafficLightSystem();
+            }
+
+            //初期化済みに
+            intersectionIsInitialized = true;
+        }
+    }
+}
