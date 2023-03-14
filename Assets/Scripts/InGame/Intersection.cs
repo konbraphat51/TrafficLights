93fd9b96ef3356e913c57dff9beb6b66b7cc5f07
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-03-15 00:07:50 +0900

    RoadJointが時計回りを保持

--- Assets/Scripts/InGame/Intersection.cs
+++ Assets/Scripts/InGame/Intersection.cs
@@ -12,63 +12,24 @@ namespace InGame
     public class Intersection : RoadJoint
     {
         [Tooltip("対応する信号機システム。nullなら信号機無しの交差点")]
-        [SerializeField] private TrafficLightsSystem? trafficLightSystem;
+        [SerializeField] private TrafficLightsSystem trafficLightSystem;
 
         /// <summary>
-        /// TrafficLightsSystemに道路、信号機を登録する
-        /// ここでTrafficLightのActivateも行われる
+        /// TrafficLightsSystemに道路、信号機を登録もする
         /// Roadが登録される前のStartに登録すると不具合が発生するので、その後に呼ぶ必要がある
         /// </summary>
-        public void InitializeTrafficLightSystem()
+        public override void ArrangeRoadsClockwise()
         {
+            base.ArrangeRoadsClockwise();
+
             //信号機が無い場合はキャンセル
-            if(trafficLightSystem == null)
+            if (trafficLightSystem == null)
             {
                 return;
             }
 
-            //時計回りにRoadを並べる
-            Road[] orderedRoads = ArrangeRoadsClockwise(connectedRoads.ToArray());
-
             //TrafficLightsSystemに登録
-            trafficLightSystem.RegisterTrafficLights(orderedRoads, edges);
-        }
-
-        /// <summary>
-        /// Intersectionから見てroadを時計回りに並べる
-        /// </summary>
-        private Road[] ArrangeRoadsClockwise(Road[] roads)
-        {
-            //各Roadとのx軸正方向からの時計回りの角度を求める
-            Dictionary<Road, float> angles = new Dictionary<Road, float>();
-            foreach(Road road in roads)
-            {
-                //RoadとIntersectionの差ベクトル
-                Vector3 dif = road.transform.position - transform.position;
-
-                //x軸正方向との符号がついていない角度（ベクトルのなす角）
-                float unsigned = Vector2.Angle(new Vector2(1, 0), dif); 
-
-                if (dif.y >= 0)
-                {
-                    //RoadがIntersectionより上側にあるとき、０～１８０度になっているところに180度足す
-                    angles[road] = 360f - unsigned;
-                }
-                else
-                {
-                    //下にある場合はそのまま
-                    angles[road] = unsigned;
-                }
-            }
-
-            //角度が小さい順に並び替え
-            List<Road> orderedRoad = new List<Road>();
-            foreach(KeyValuePair<Road, float> roadAngle in angles.OrderBy(c => c.Value))
-            {
-                orderedRoad.Add(roadAngle.Key);
-            }
-
-            return orderedRoad.ToArray();
+            trafficLightSystem.RegisterTrafficLights(connectedRoads.ToArray(), edges);
         }
     }
 }
